3bc3d85af27fec8dadd58ede2ba505bd865421e1
Marius Merkevicius <marius.m@ito.lt>
2022-06-25 08:34:40 +0300

    Playing around on module registration

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.6.21"
+    id("de.jjohannes.extra-java-module-info")  version "0.14"
     application
 }
 
@@ -32,9 +33,40 @@ tasks.test {
 }
 
 tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "1.8"
+    kotlinOptions.jvmTarget = "11"
 }
 
 application {
-    mainClass.set("lt.markmerkk.MainKt")
+    mainClass.set("lt.markmerkk.mp3cutter.MainKt")
+}
+
+// Source: https://plugins.gradle.org/plugin/de.jjohannes.extra-java-module-info
+// Source: https://github.com/jjohannes/extra-java-module-info
+extraJavaModuleInfo {
+    // ffmpeg
+    automaticModule("ffmpeg-0.7.0.jar", "net.bramp.ffmpeg")
+    automaticModule("modelmapper-3.1.0.jar", "modelmapper")
+    automaticModule("annotations-13.0.jar", "annotations")
+    automaticModule("failureaccess-1.0.1.jar", "failureaccess")
+    automaticModule("jsr305-3.0.2.jar", "jsr305")
+    automaticModule("j2objc-annotations-1.3.jar", "j2objc.annotations")
+    automaticModule(
+        "listenablefuture-9999.0-empty-to-avoid-conflict-with-guava.jar",
+        "listenablefuture.9999.0.empty.to.avoid.conflict.with.guava"
+    )
+
+    // Logging
+    automaticModule("jul-to-slf4j-1.7.36.jar", "jul.to.slf4j")
+    automaticModule("logback-classic-1.2.11.jar", "logback.classic")
+    automaticModule("logback-core-1.2.11.jar", "logback.core")
+
+    // Kotlin
+    automaticModule("kotlinx-cli-jvm-0.3.4.jar", "kotlinx.cli.jvm")
+    automaticModule("kotlin-stdlib-common-1.6.21.jar", "kotlin.stdlib.common")
+
+    // Tests
+    automaticModule("mockito-kotlin-1.5.0.jar", "mockito.kotlin")
+    automaticModule("assertj-core-3.8.0.jar", "assertj.core")
+    automaticModule("kotlin-reflect-1.0.7.jar", "kotlin.reflect")
+    automaticModule("objenesis-2.6.jar", "objenesis")
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,10 +2,10 @@ module lt.markmerkk.mp3cutter {
     requires java.net.http;
     requires java.logging;
     requires com.google.common;
-    requires ffmpeg;
     requires org.slf4j;
     requires kotlin.stdlib;
+    requires org.apache.commons.io;
     requires jul.to.slf4j;
     requires kotlinx.cli.jvm;
-    requires org.apache.commons.io;
+    requires net.bramp.ffmpeg;
 }

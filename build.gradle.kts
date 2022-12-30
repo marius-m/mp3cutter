5020f3d8371ed12c4671e21050fd684eb11cc8e5
Marius Merkevicius <merkevicius.marius@gmail.com>
2022-12-30 14:05:39 +0200

    Update testing framework

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,9 +23,10 @@ dependencies {
     implementation("ch.qos.logback:logback-classic:1.2.11")
 
     testImplementation("org.jetbrains.kotlin:kotlin-test")
-    testImplementation("org.mockito:mockito-core:2.23.0")
     testImplementation("org.assertj:assertj-core:3.8.0")
-    testImplementation("com.nhaarman:mockito-kotlin:1.5.0")
+    testImplementation("io.mockk:mockk:1.13.3")
+//    testImplementation("org.mockito:mockito-core:2.23.0")
+//    testImplementation("com.nhaarman:mockito-kotlin:1.5.0")
 }
 
 tasks.test {
@@ -63,10 +64,19 @@ extraJavaModuleInfo {
     // Kotlin
     automaticModule("kotlinx-cli-jvm-0.3.4.jar", "kotlinx.cli.jvm")
     automaticModule("kotlin-stdlib-common-1.6.21.jar", "kotlin.stdlib.common")
+    automaticModule("kotlin-stdlib-common-1.7.20.jar", "kotlin.stdlib.common")
+    automaticModule("kotlinx-coroutines-core-jvm-1.6.4.jar", "kotlin.coroutines.core")
 
     // Tests
     automaticModule("mockito-kotlin-1.5.0.jar", "mockito.kotlin")
     automaticModule("assertj-core-3.8.0.jar", "assertj.core")
     automaticModule("kotlin-reflect-1.0.7.jar", "kotlin.reflect")
     automaticModule("objenesis-2.6.jar", "objenesis")
+    automaticModule("hamcrest-core-1.3.jar", "hamcrest")
+    automaticModule("mockk-jvm-1.13.3.jar", "mockk")
+    automaticModule("mockk-dsl-1.13.3.jar", "mockk")
+    automaticModule("mockk-dsl-jvm-1.13.3.jar", "mockk")
+    automaticModule("mockk-agent-jvm-1.13.3.jar", "mockk")
+    automaticModule("mockk-agent-api-jvm-1.13.3.jar", "mockk")
+    automaticModule("mockk-core-jvm-1.13.3.jar", "mockk")
 }
